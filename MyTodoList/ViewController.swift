45c19aa9145d8a171433cbdcb52be13840afccf1
BrayanKellyBalbuena <the.script07@gmail.com>
2019-01-06 14:28:55 -0400

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by Brayan Kelly Balbuena on 1/6/19.
+//  Copyright © 2019 Brayan Kelly Balbuena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
